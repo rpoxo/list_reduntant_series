f3eb54bed11dfc1e4a2213b1813edb26c7b5435e
Nikita <gotsko@nksupport.com>
2021-07-24 17:20:33 +0300

    add pattern, date scan

--- find_reduntant_backups.py
+++ find_reduntant_backups.py
@@ -12,13 +12,31 @@ import os
 import logging
 import argparse
 import re
+import datetime
+from datetime import datetime, timedelta
 
-def filter_current_month(path):
+pattern_date = r'(?P<name>.*)-(?P<day>\d\d)-(?P<month>\S{3})-(?P<year>\d\d\d\d)-(?P<hour>\d\d):(?P<minute>\d\d).*'
 
+def parse_filenames(path):
+    logging.debug(f'listing files in {path}')
+    files = [item for item in os.listdir(path) if os.path.isfile(os.path.join(path, item))]
+    backups = {}
+    for fpath in files:
+        logging.debug(f'testing {fpath}')
+        match = re.search(pattern_date, fpath)
+        if match:
+            name, day, month, year, hour, minute = match.groups()
+            logging.info(f'Matched [{name}] {fpath}: {match.groups()}')
+            dt = datetime.strptime(''.join([day, month, year, hour, minute]), '%d%b%Y%H%M')
+            if name not in backups.keys(): backups[name] = {}
+            backups[name][dt] = fpath
+
+def filter_current(path):
+    parse_filenames(path)
+    
 
 def main(args):
-    if args.policy-current:
-        print(1)
+    if args.current: filter_current(args.dir)
 
 def setup_logging(args):
     handlers = []
@@ -54,7 +72,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("dir", help="Path to directory with backups")
     parser.add_argument("-v", "--verbose", help="Set verbosity level", action='count', default=1)
-    parser.add_argument("--policy-current", help="List backups reduntant backups from current 2 month", action='store_true')
+    parser.add_argument("--current", help="List backups reduntant backups from current 2 month", action='store_true')
     args = parser.parse_args()
 
     return args
