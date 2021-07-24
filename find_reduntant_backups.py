0a3d3646860245d4db8a3cfaf657341c8f9429bf
Nikita <gotsko@nksupport.com>
2021-07-24 19:38:10 +0300

    add excluding already marked items

--- find_reduntant_backups.py
+++ find_reduntant_backups.py
@@ -17,9 +17,11 @@ from datetime import datetime, timedelta, date, time
 
 pattern_date = r'(?P<name>.*)-(?P<day>\d\d)-(?P<month>\S{3})-(?P<year>\d\d\d\d)-(?P<hour>\d\d):(?P<minute>\d\d).*'
 
-def parse_filenames(path):
-    logging.debug(f'listing files in {path}')
+def parse_filenames(path, reduntant):
+    logging.debug(f'listing files in {path}, excluding {reduntant}')
     files = [item for item in os.listdir(path) if os.path.isfile(os.path.join(path, item))]
+    files = [fname for fname in files if os.path.join(path, fname) not in reduntant]
+    logging.debug(f'files = {files}')
     backups = {}
     for fname in files:
         logging.debug(f'testing {fname}')
@@ -36,17 +38,16 @@ def parse_filenames(path):
             backups[name][year][month][day][dt] = fname
     return backups
 
-def filter_last(path):
+def filter_last(path, reduntant, daily_limit):
     '''
         Will display backups with more than 3 files per day for current and previous month(starting from 1st)
     '''
-    daily_limit = 3
 
     today = datetime.combine(date.today(), time())
     last_month = today.replace(month=today.month-1)
     last_month_start = last_month.replace(day=1)
 
-    backups = parse_filenames(path)
+    backups = parse_filenames(path, reduntant)
 
     for name, years in backups.items():
         for year, months in years.items():
@@ -56,32 +57,42 @@ def filter_last(path):
                     if len(filtered) > daily_limit:
                         logging.warning(f'more than {daily_limit} backups found for {year}-{month}-{day}')
                         for fname in filtered[daily_limit:]:
-                            print(os.path.join(path, fname))
+                            logging.info(f'marking {fname} as reduntant')
+                            return(os.path.join(path, fname))
 
-def filter_older(path, daily_limit, days):
+def filter_older(path, reduntant, daily_limit, days):
     # NOTE: timedelta does not have month argument, as it needs to be datetime-aware for that
     td = timedelta(days=days)
     today = datetime.combine(date.today(), time())
     this_year = today.replace(month=1)
     this_year_start = this_year.replace(day=1)
 
-    backups = parse_filenames(path)
+    backups = parse_filenames(path, reduntant)
 
     for name, years in backups.items():
         for year, months in years.items():
             for month, days in months.items():
                 for day, datetimes in days.items():
-                    filtered = [fname for dt, fname in datetimes.items() if dt > this_year and dt+td < datetime.combine(date.today(), time())]
+                    filtered = [fname for dt, fname in datetimes.items() if dt > this_year and dt < datetime.combine(date.today(), time()) - td]
                     if len(filtered) > daily_limit:
                         logging.warning(f'more than {daily_limit} backups found for {year}-{month}-{day}')
                         for fname in filtered[daily_limit:]:
-                            print(os.path.join(path, fname))
+                            logging.info(f'marking {fname} as reduntant, older than {datetime.combine(date.today(), time())} - {td} = {datetime.combine(date.today(), time()) - td}')
+                            return(os.path.join(path, fname))
     
 
 def main(args):
-    if args.last: filter_last(args.dir)
-    if args.older: filter_older(args.dir, 2, 60)
-    if args.old: filter_older(args.dir, 1, 90)
+    reduntant = []
+    if args.last: reduntant.append(filter_last(args.dir, reduntant, 3))
+    elif args.older: reduntant.append(filter_older(args.dir, reduntant, 2, 60))
+    elif args.old: reduntant.append(filter_older(args.dir, reduntant, 1, 90))
+    else:
+        reduntant.append(filter_last(args.dir, reduntant, 3))
+        reduntant.append(filter_older(args.dir, reduntant, 2, 60))
+        reduntant.append(filter_older(args.dir, reduntant, 1, 90))
+    
+    for fpath in set(reduntant):
+        print(fpath)
 
 def setup_logging(args):
     handlers = []
